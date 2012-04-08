6b0ffd37db7590b1db074d5b7568413f3ac59a36
nna@nna774.net <nna@nna774.net>
2012-04-09 01:05:56 +0900

    0000

---
+++ 0000.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(int,char**){
+	for(int i(1);i<=9;++i)
+		for(int j(1);j<=9;++j) std::cout << i << 'x' << j << '=' << i*j << std::endl;
+	return 0;
+}
