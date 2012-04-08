78aa9085d1025db514c1f3eacdc13bd330f75fa7
nna@nna774.net <nna@nna774.net>
2012-04-09 01:26:34 +0900

    0001 解いた

--- 0001.cpp
+++ 0001.cpp
@@ -3,6 +3,9 @@
 #include <algorithm>
 
 int main(int,char**){
-
+	std::vector<int> vec(10);
+	for(int i(0);i<10;++i) std::cin >> vec[i];
+	std::sort(vec.begin(),vec.end());
+	std::cout << vec[9] << std::endl<< vec[8] << std::endl<< vec[7] << std::endl;
 	return 0;
 }
