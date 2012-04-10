3c56f62d588e8d6592bb60f61c7c691873e57bd1
nna@nna774.net <nna@nna774.net>
2012-04-10 17:03:47 +0900

    いろいろ

---
+++ Volume_5/0500-0509/0502.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <string>
+#include <set>
+#include <cmath>
+#include <map>
+
+
+int main(int,char**){
+	int n;
+	while(std::cin >> n){
+		if(!n) break;
+		// データセットごとの変数宣言
+		while(n--){
+			// データセットごとの処理
+		}
+		// 1データセット終了時
+	}
+	return 0;
+}
